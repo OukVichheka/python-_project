ba140950bf48d477e8faeffe6d3d510178351ef9
OukVichheka <42796794+OukVichheka@users.noreply.github.com>
2019-02-28 14:41:09 +0700

    Delete Book.py

--- Book.py
+++
@@ -1,9 +0,0 @@
-import json
-
-file_book = open('book.json','r')
-
-book_data = json.load(file_book)
-for content in book_data['Content']:
-    for chapter in content:                           
-        print(content)
-        print(chapter)
